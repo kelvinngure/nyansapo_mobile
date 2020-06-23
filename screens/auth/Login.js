86bb0876fffe019db0aedd0b40ecee05fe132d68
kelvinngure <njuguna.kelvin22@gmail.com>
2020-06-23 11:24:25 -0400

    add

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -1,9 +1,12 @@
 import React, {useContext} from "react"
 import { TouchableOpacity, View, Text, Image, TextInput } from "react-native";
 import styles from "../../styles/styles"
+import Colors from "../../styles/Colors"
 
 
-const Login = () => {
+
+
+const Login = ({navigation}) => {
 
     return (
         <View style = {styles.loginScreen}>
@@ -11,14 +14,20 @@ const Login = () => {
                 <Image source = {require("../../assets/logo.png")} style = {styles.authlogo}/>
             </View>
             <View style = {styles.inputView}>
-                <TextInput style = {styles.authInputs}></TextInput>
-                <TextInput style = {styles.authInputs}></TextInput>
+                <TextInput style = {styles.authInputs} placeholder ="email" placeholderTextColor={Colors.appBrown}></TextInput>
+                <TextInput style = {styles.authInputs} placeholder ="password" placeholderTextColor={Colors.appBrown} secureTextEntry= {true}></TextInput>
             </View>
             <View style = {styles.authButtonView}>
+                <TouchableOpacity style = {styles.forgot}>
+                    <Text style = {styles.authButtonText}>forgot password?</Text>
+                </TouchableOpacity>
                 <TouchableOpacity style = {styles.authButton}>
-                    <Text style = {styles.authButtonText}>sign in</Text>
+                    <Text style = {styles.signin}>sign in</Text>
                 </TouchableOpacity>
             </View>
+            <TouchableOpacity onPress = {() => navigation.navigate("Register")} >
+                <Text>CREATE ACCOUNT</Text>
+            </TouchableOpacity>
         </View>
     )
 }
