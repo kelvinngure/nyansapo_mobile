0150a2a274e8716c49a2e1d0c98d227e9cfc6af2
kelvinngure <njuguna.kelvin22@gmail.com>
2020-06-25 19:15:02 -0400

    Update Home.js

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,19 +1,34 @@
 import React from "react"
 import { View, Text } from "react-native"
 import styles from "../../styles/styles"
-import { TouchableOpacity } from "react-native-gesture-handler"
+import { createStackNavigator } from '@react-navigation/stack';
 import Header from "./Header"
 import History from "./History"
+import CreateStudent from "./CreateStudent";
 
+const Stack = createStackNavigator();
 
-const Home = () => {
+const HomeTab = ({navigation}) => {
     return(
-        <View styles = {styles.tabs}>
-            <Header />
+        <View>
+            <Header navigation = {navigation} />
             <History />
         </View>
     )
 }
+
+
+
+function Home({navigation}) {
+  return (
+    <Stack.Navigator screenOptions={{animationEnabled: false}}>
+        <Stack.Screen name="Home" component={HomeTab} options={{headerShown: false}}/>
+        <Stack.Screen name="Create" component={CreateStudent}  options={{headerShown: false}}/>
+    </Stack.Navigator>
+    
+  );
+}
+
 
 
 
