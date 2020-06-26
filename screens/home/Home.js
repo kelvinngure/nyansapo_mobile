7ed4f3cfafc21bba59184f012781aca81ca01a6a
kelvinngure <njuguna.kelvin22@gmail.com>
2020-06-26 15:08:40 -0400

    Update Home.js

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,18 +1,36 @@
-import React from "react"
+import React, {useState} from "react"
 import { View, Text } from "react-native"
 import styles from "../../styles/styles"
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from "./Header"
 import History from "./History"
 import CreateStudent from "./CreateStudent";
+import { SearchBar } from 'react-native-elements';
+
 
 const Stack = createStackNavigator();
 
 const HomeTab = ({navigation}) => {
+    const [search, setSearch] = useState("dsd")
+
+    const updateSearch = (text) => {
+      setSearch(text)
+    }
+
+    const sendSearch = () => {
+      console.log(search)
+    }
+
     return(
-        <View>
-            <Header navigation = {navigation} />
-            <History />
+        <View style = {styles.homeCombine}>
+             <SearchBar
+                  placeholder="Search for student..."
+                  onChangeText={(text) => updateSearch(text)}
+                  value={search}
+                  onSubmitEditing = {sendSearch}
+                  platform = "android"
+              />
+            <History navigation = {navigation}/>
         </View>
     )
 }
