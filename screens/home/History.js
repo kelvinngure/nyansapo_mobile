03a52c31f19f3493673fbdb768c6d5f2db8b9c4e
kelvinngure <njuguna.kelvin22@gmail.com>
2020-06-26 15:08:48 -0400

    Update History.js

--- screens/home/History.js
+++ screens/home/History.js
@@ -1,22 +1,30 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
 import styles from "../../styles/styles"
+import {IconBar, Search, Menu, Add} from "../../styles/Icons"
+
 
 
 const Searched = () => {
     return(
         <View>
-            <Text>Result</Text>
+            <Text>Resu</Text>
         </View>
     )
 }
 
-const History = () => {
+const History = ({navigation}) => {
     return(
-        <View style = {styles.history}>
-            <Searched />
-            <Searched />
-            <Searched />
+        <View style = {styles.hometab}>
+            <View style = {styles.results}>
+                <Searched />
+                <Searched />
+                <Searched />
+            </View>
+            
+            <View style = {styles.icons}>
+                <TouchableOpacity onPress = {() => navigation.navigate("Create")}><Add /></TouchableOpacity>
+            </View>
 
         </View>
     )
